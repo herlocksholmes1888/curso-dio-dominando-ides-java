559a15d02df638ea5983586ca292530d521ed9c3
Jay <luisafrugoli11@gmail.com>
2022-02-16 13:11:38 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package com.dio;
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato(null, null, null);
+		
+		System.out.println(gato);
+		/* System.out.println("Hello, world!"); */
+	}
+}
